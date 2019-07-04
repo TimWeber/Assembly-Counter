7e188bc80cc122a17d03b30e727f3fac8e983cca
Tim Weber <43787505+TimWeber@users.noreply.github.com>
2019-07-04 15:49:51 +0100

    Update second.asm

--- second.asm
+++ second.asm
@@ -2,33 +2,33 @@
 ; Author:		Tim Weber
 ;Date:			120319
 ;**********************************SETUP***************************
-include"P16F877.INC" 		; This tells the assembler to 'include'
-							; another source file
-INNER	EQU H'0020' 		; register at address 20h is named INNER
-OUTER	EQU	H'0021'			; register at address 21h is named OUTER
-delval	EQU H'00FF' 		; this is the delay time value 255
-							; to allow you to single step around
+include"P16F877.INC" 			; This tells the assembler to 'include'
+							
+INNER		EQU 	H'0020' 	; register at address 20h is named INNER
+OUTER		EQU	H'0021'		; register at address 21h is named OUTER
+delval		EQU 	H'00FF' 	; this is the delay time value 255
+
 ;*******************************PORT SETUP*************************
 		banksel	TRISD 		;change register bank (page)
 		clrf 	PORTD 		; make PORTD an output port
 		banksel	PORTD		; change back to original bank
 ;************************************MAIN**************************
-start 	movlw 0 			; move 'literal' value 0 into the W register
+start 		movlw 	0 		; move 'literal' value 0 into the W register
 		movwf 	PORTD 		; clear LEDs on PORTD
-loop 	incf 	PORTD,F		; update the LEDs by incrementing them
+loop 	incf 	PORTD,F			; update the LEDs by incrementing them
 		call	delay 		; Call delay subroutine
-		nop					; do nothing at all for one machine cycle
+		nop			; do nothing at all for one machine cycle
 		call	hex
- 							; except increment the program counter
+
 		goto	loop		; go back to the instruction labelled 'loop'
 ;******************************DELAY SUBROUTINE****************
-delay	movlw 	delval 		; load w with delay time value
+delay	movlw 	delval 			; load w with delay time value
 		movwf	OUTER		; and put it into register for OUTER
-delay1	movlw	delval		; load the working register with the delay value
+delay1	movlw	delval			; load the working register with the delay value
 		movwf	INNER		; put it into the register for INNER
-delay2	decfsz	INNER, F	; count down one and skip if the number is 0
+delay2	decfsz	INNER, 		F	; count down one and skip if the number is 0
 		goto	delay2		; go back and count down one until 0
-		decfsz	OUTER, F	; count down one and skip if at 0
+		decfsz	OUTER, 	F	; count down one and skip if at 0
 		goto	delay1		
-		return				; return to call point
-		end					; note that end is the very last line of code
+		return			; return to call point
+		end			; note that end is the very last line of code
